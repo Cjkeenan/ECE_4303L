6aa2a6a9cfefa6cef6a7bd3d4c70850b83831099
cjkeenan <cjkeenan@live.com>
2020-02-05 21:26:49 -0800

    Additional Python Client Features

--- Lab 1/Part 2/Python/client.py
+++ Lab 1/Part 2/Python/client.py
@@ -2,8 +2,16 @@
 import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # addr = socket.gethostname()
-addr = '192.168.137.174'
-server.connect((addr,1000))
+# addr = '192.168.137.174'
+addr = '127.0.0.1'
+port = 4000
+server.connect((addr,port))
 
-msg = server.recv(1024)
-print(msg.decode("utf-8"))
+key = -1
+while(True):
+    msgReceived = server.recv(1024)
+    print("Server >> ", msgReceived.decode("utf-8"))
+    msgSent = input()
+    server.send(msgSent.encode("utf-8"))
+server.close()
+print("Connection Terminated!")
