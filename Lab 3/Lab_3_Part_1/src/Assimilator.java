e2be6d9ffc89b70486a9aef415c7ecd470853baf
cjkeenan <cjkeenan@live.com>
2020-02-12 14:11:56 -0800

    Lab 3 Initial commit
    Part 1 working (Using VS Code)

---
+++ Lab 3/Lab_3_Part_1/src/Assimilator.java
@@ -0,0 +1,44 @@
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+import java.io.*;
+
+public class Assimilator {
+
+  public static void main(String[] args) {
+    final String username = "cjkeenan@live.com";
+    final String password = "drjyymaozmymfqsg";
+
+    Properties props = new Properties();
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.host", "outlook.office365.com");
+    props.put("mail.smtp.port", "587");
+
+    Session session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+        }
+      });
+
+    try {
+      System.out.println("Enter recipient's email address: ");
+      String recipient = System.console().readLine();
+      Message message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(username));
+      message.setRecipients(Message.RecipientType.TO,
+          InternetAddress.parse(recipient));
+      message.setContent("Resistance is futile. You will be assimilated!", "text/plain");
+      message.setSubject("You must comply.");
+
+      Transport.send(message);
+
+      System.out.println("Done sending email to " + recipient);
+    }
+    catch (Exception ex) {
+      ex.printStackTrace(); 
+    }
+    
+  }
+}
