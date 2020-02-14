bf9dc900c0672ed11bb114f78923da6fa63fb41b
Cjkeenan <cjkeenan@live.com>
2020-02-13 16:51:49 -0800

    Code Cleanup
    Unecessary java files deleted in project folders. all files still exist in the distribution folder.

--- Lab 3/Mail Client/src/Assimilator.java
+++ Lab 3/Mail Client/src/Assimilator.java
@@ -11,7 +11,7 @@ public class Assimilator {
     Properties props = new Properties();
     props.put("mail.smtp.auth", "true");
     props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.smtp.host", "outlook.office365.com");
+    props.put("mail.smtp.host", "smtp.office365.com");
     props.put("mail.smtp.port", "587");
 
     Session session = Session.getInstance(props, new MailAuthenticator(username));
