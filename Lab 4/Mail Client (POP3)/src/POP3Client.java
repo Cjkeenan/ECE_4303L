60cabdabebfbf34ad63a063f2c83179b645cd84a
cjkeenan <cjkeenan@live.com>
2020-02-19 16:29:18 -0800

    fixed program not closing on completion of tasks

--- Lab 4/Mail Client (POP3)/src/POP3Client.java
+++ Lab 4/Mail Client (POP3)/src/POP3Client.java
@@ -5,51 +5,54 @@ import java.io.FileWriter;
 
 public class POP3Client {
 
-  public static void main(String[] args) {
+public static void main(String[] args) {
+  
+  String provider = "pop3";
+  String host = "outlook.office365.com";
+
+  Properties props = new Properties();
+  props.put("mail.pop3.auth", "true");
+  props.put("mail.pop3.ssl.enable", "true");
+  props.put("mail.pop3.host", host);
+  props.put("mail.pop3.port", "995");
+
+  try {
+
+    // Connect to the POP3 server
+    Session session = Session.getDefaultInstance(props, new MailAuthenticator());
+    Store store = session.getStore(provider);
+    store.connect(host, null, null);
+    // store.connect();
     
-    Properties props = new Properties();
-    props.put("mail.pop3.auth", "true");
-    props.put("mail.pop3.ssl.enable", "true");
-    props.put("mail.pop3.host", "outlook.office365.com");
-    props.put("mail.pop3.port", "995");
+    // Open the folder
+    Folder inbox = store.getFolder("INBOX");
+    if (inbox == null) {
+      System.out.println("No INBOX");
+      System.exit(1);
+    }  
+    inbox.open(Folder.READ_ONLY);
+    
+    // Get the messages from the server
+    // Message[] messages = inbox.getMessages(1, 20);
+    Message[] messages = inbox.getMessages();
+    FileWriter myWriter = new FileWriter("messages.html");
+    for (int i = messages.length - 1; i >= 0; i--) {
+      // System.out.println("------------ Message " + (i+1) + " ------------");
+      // messages[i].writeTo(System.out);
+      myWriter.write("<br><br>------------ Message " + (i+1) + " ------------<br>");
+      myWriter.write(messages[i].getContent().toString());
+    }
+    myWriter.close();
 
-    String provider = "pop3";
-
-    try {
-
-      // Connect to the POP3 server
-      Session session = Session.getDefaultInstance(props, new MailAuthenticator());
-      Store store = session.getStore(provider);
-      // store.connect(host, username, password);
-      store.connect();
-      
-      // Open the folder
-      Folder inbox = store.getFolder("INBOX");
-      if (inbox == null) {
-        System.out.println("No INBOX");
-        System.exit(1);
-      }  
-      inbox.open(Folder.READ_ONLY);
-      
-      // Get the messages from the server
-      // Message[] messages = inbox.getMessages(1, 20);
-      Message[] messages = inbox.getMessages();
-      FileWriter myWriter = new FileWriter("messages.html");
-      for (int i = messages.length - 1; i >= 0; i--) {
-        // System.out.println("------------ Message " + (i+1) + " ------------");
-        // messages[i].writeTo(System.out);
-        myWriter.write("<br><br>------------ Message " + (i+1) + " ------------<br>");
-        myWriter.write(messages[i].getContent().toString());
-      }
-      myWriter.close();
-
-      // Close the connection 
-      // but don't remove the messages from the server
-      inbox.close(false);
-      store.close();  
-    } 
-    catch (Exception ex) {
-      ex.printStackTrace();
-    }    
+    // Close the connection 
+    // but don't remove the messages from the server
+    inbox.close(false);
+    store.close();  
+  } 
+  catch (Exception ex) {
+    ex.printStackTrace();
+  }
+    // since we brought up a GUI returning from main() won't exit
+    System.exit(0);   
   }
 }
