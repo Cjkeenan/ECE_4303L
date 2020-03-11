24ba520259023b3aa02f5a1ace7fcda285f678c1
cjkeenan <cjkeenan@live.com>
2020-03-11 13:01:14 -0700

    Update DownloadAttachments.java
    Cleaned up formatting

--- Lab 6 - Mail Attachments/Receiving Attachments/src/DownloadAttachments.java
+++ Lab 6 - Mail Attachments/Receiving Attachments/src/DownloadAttachments.java
@@ -20,11 +20,10 @@ import java.io.IOException;
 import javax.mail.Part;
 
 
-public class DownloadAttachments {
+public class FolderManagement {
    public static void main(String args[]) 
    {
       Date date = null;
-
       
       Properties props = new Properties();
       props.put("mail.pop3.auth", "true");
@@ -37,18 +36,15 @@ public class DownloadAttachments {
       props.put("mail.smtp.starttls.enable", "true");
       
       
-      
-
       // session.setDebug(true);
       try 
       {
-
-
-    	 // Connect to the POP3 server
-    	 Session session = Session.getDefaultInstance(props, new MailAuthenticator());
-    	 Store store = session.getStore("pop3s");
-    	 store.connect();
-    	 String saveDirectory = "C:\\Users\\eunsu\\Documents\\GitHub\\ECE_4303L\\Lab 6 - Mail Attachments\\Receiving Attachments\\Attachments";  
+    	   // Connect to the POP3 server
+    	   Session session = Session.getDefaultInstance(props, new MailAuthenticator());
+    	   Store store = session.getStore("pop3s");
+         store.connect();
+        
+         String saveDirectory = System.getProperty("user.dir") + "\\Attachments";  
          Folder folder = store.getFolder("inbox");
          if (!folder.exists()) {
             System.out.println("inbox not found");
@@ -63,97 +59,108 @@ public class DownloadAttachments {
          if (messages.length != 0) {
 
             //for (int i = 0, n = messages.length; i < n; i++) 
-        	 OUTER_LOOP:
-        	 for (int i = messages.length - 1; i >= 0; i--)
-        	 {
+         OUTER_LOOP:
+         for (int i = messages.length - 1; i >= 0; i--)
+         {
                Message message = messages[i];
+               String messageContent = "";
+
                date = message.getSentDate();
+               String contentType = message.getContentType();
+
                // Get all the information from the message
                String from = InternetAddress.toString(message.getFrom());
-               String contentType = message.getContentType();
-               String messageContent = "";
                if (from != null) {
                   System.out.println("From: " + from);
                }
-               String to = InternetAddress.toString(message
-	         .getRecipients(Message.RecipientType.TO));
+
+               String to = InternetAddress.toString(message.getRecipients(Message.RecipientType.TO));
                if (to != null) {
                   System.out.println("To: " + to);
                }
+
                String subject = message.getSubject();
                if (subject != null) {
                   System.out.println("Subject: " + subject);
                }
+
                Date sent = message.getSentDate();
                if (sent != null) {
                   System.out.println("Sent: " + sent);
                }
-               System.out.println("Message : ");
 
+               System.out.println("Message : ");
                Multipart multipart = (Multipart) messages[i].getContent();
                
-               for (int x = 0; x < multipart.getCount(); x++) {
-               BodyPart bodyPart = multipart.getBodyPart(x);
+               for (int x = 0; x < multipart.getCount(); x++) 
+               {
+                  BodyPart bodyPart = multipart.getBodyPart(x);
 
-               String disposition = bodyPart.getDisposition();
-               System.out.println(bodyPart.getContent());
-               if (disposition != null && (disposition.equals(BodyPart.ATTACHMENT))) {
-               System.out.println("Mail have some attachment");
+                  String disposition = bodyPart.getDisposition();
+                  System.out.println(bodyPart.getContent());
+                  if (disposition != null && (disposition.equals(BodyPart.ATTACHMENT))) 
+                  {
+                     System.out.println("Mail have some attachment");
+                  }
                }
-               }
+
                String attachFiles = "";
                if (contentType.contains("multipart")) {
-                   // content may contain attachments
-                   Multipart multiPart = (Multipart) message.getContent();
-                   int numberOfParts = multiPart.getCount();
-                   for (int partCount = 0; partCount < numberOfParts; partCount++) {
-                       MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-                       if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+                  // content may contain attachments
+                  Multipart multiPart = (Multipart) message.getContent();
+                  int numberOfParts = multiPart.getCount();
+                  for (int partCount = 0; partCount < numberOfParts; partCount++) 
+                  {
+                     MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+                     if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) 
+                     {
                            // this part is attachment
                            String fileName = part.getFileName();
                            attachFiles += fileName + ", ";
                            part.saveFile(saveDirectory + File.separator + fileName);
-                       } else {
+                     } 
+                     else 
+                     {
                            // this part may be the message content
                            messageContent = part.getContent().toString();
-                       }
-                   }
+                     }
+                  }
 
-                   if (attachFiles.length() > 1) {
-                       attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
-                   }
-               } else if (contentType.contains("text/plain")
-                       || contentType.contains("text/html")) {
-                   Object content = message.getContent();
-                   if (content != null) {
-                       messageContent = content.toString();
-                   }
-               }              
-
-               
-
+                  if (attachFiles.length() > 1) 
+                  {
+                     attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
+                  }
+               }
+               else if (contentType.contains("text/plain") || contentType.contains("text/html")) 
+               {
+                  Object content = message.getContent();
+                  if (content != null) 
+                  {
+                     messageContent = content.toString();
+                  }
+               }
 
                System.out.print("Do you want to view next message? [y/n] : ");
                String ans = reader.readLine();
-               if ("N".equals(ans) || "n".equals(ans)) {
-            	   System.out.print("Quiting email viewer program...");
-            	   break OUTER_LOOP;
-            	   
-               }
+               if ("N".equals(ans) || "n".equals(ans)) 
+                  {
+                     System.out.print("Quiting email viewer program...");
+                     break OUTER_LOOP;
+                  }
                } 
-           //end of for loop
+         //end of for loop
 
          }
-         else {
+         else 
+         {
             System.out.println("There is no msg....");
             folder.close(false);
             store.close();
          }
-
-      } catch (Exception e) {
+      }
+      catch (Exception e)
+      {
          e.printStackTrace();
       }
-
    }
-
 }
